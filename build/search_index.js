4033a1a362cc2b8b0ca22cc0d809e0ac082cce31
T. Yasusi <yasusi.1530@gmail.mail>
2025-07-30 13:55:54 +0900

    Initialize GitHub Pages

---
+++ build/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#このパッケージの概要","page":"Home","title":"このパッケージの概要","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"このパッケージはjuliaの教育的なパッケージを目指している。","category":"page"}]
+}
